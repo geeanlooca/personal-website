63a73b847ff282f441fa601c5ecbf72beb7ea225
Gianluca Marcon <marcon.gluca@gmail.com>
2026-07-10 10:04:00 +0000

    feat: add content-hash cache-busting for CSS (v1.1.0)

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode("cssVersion", () => {
+    const css = fs.readFileSync("./src/css/style.css", "utf8");
+    return crypto.createHash("md5").update(css).digest("hex").slice(0, 8);
+  });
 
   return {
     dir: {
