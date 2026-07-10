ddfe276795009dcb2bdf72abb5042fa8d42094b5
Gianluca Marcon <marcon.gluca@gmail.com>
2026-07-10 16:00:30 +0000

    feat(site): add manim passthrough copy and shortcode, replace svg with animation

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,18 @@ const crypto = require("crypto");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/favicon.svg");
+  eleventyConfig.addPassthroughCopy({
+    "./animations/gmanim/dist": "animations"
+  });
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addShortcode("cssVersion", () => {
     const css = fs.readFileSync("./src/css/style.css", "utf8");
     return crypto.createHash("md5").update(css).digest("hex").slice(0, 8);
+  });
+  eleventyConfig.addShortcode("manim", function(name, width, height) {
+    const theme = this.ctx?.theme || "dark";
+    return `<iframe src="/animations/${name}/index.html?theme=${theme}" width="${width}" height="${height}" frameborder="0" style="display:block;margin:0 auto;" scrolling="no" allowfullscreen></iframe>`;
   });
 
   return {
